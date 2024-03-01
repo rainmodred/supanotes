f1c27bf637fd37cf68366a9e8214ac031e0d6307
rainmodred <albphoenix@gmail.com>
2024-03-01 20:04:19 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Button } from '@/components/ui/button';
+
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const url = import.meta.env.VITE_URL;
+const anonKey = import.meta.env.VITE_ANON_KEY;
+if (!url || !anonKey) {
+  throw new Error('something went wrong');
+}
+
+const supabase = createClient(url, anonKey);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button onClick={() => setCount(count => count + 1)}>
+          count is {count}
+        </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;

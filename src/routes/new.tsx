eeb6c5f8224d4b7f25b03a2abaa45f8a17f6a70d
rainmodred <albphoenix@gmail.com>
2024-08-09 19:30:17 +0300

    Fix padding

--- src/routes/new.tsx
+++ src/routes/new.tsx
@@ -30,12 +30,12 @@ export function NewNote() {
   return (
     <div className="flex h-full flex-col">
       <Form method="post" className="h-full">
-        <div className="mb-1 flex gap-1">
+        <div className="mb-1 flex gap-1 px-2">
           {session && (
             <input name="userId" value={session?.user.id} type="hidden" />
           )}
           <Input name="title" placeholder="title" required />
-          <Button variant="default" className="shrink-0">
+          <Button variant="secondary" className="shrink-0">
             Create Note
           </Button>
         </div>
